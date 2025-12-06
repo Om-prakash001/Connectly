614fc832078dc6391a9f7a565f13fa90557d9f12
Om-prakash001 <omprakash0001>
2025-12-06 21:51:23 +0530

    Project Completed

--- backEnd/src/controllers/authController.js
+++ backEnd/src/controllers/authController.js
@@ -88,7 +88,7 @@ export const logout = (req, res) => {
     res.cookie("jwt", "", {maxAge:0});
     res.status(200).json({message: "Logged out successfully"});
   } catch (err){ 
-    console.log(" Error in login controller", err.message);
+    console.log(" Error in logout controller", err.message);
     res.status(500).json({ message: "Internal Server Error" });
   }
 };
